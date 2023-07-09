1be4bbee82eec15b44b889efca6cf23839c6c2bd
Yongyan Deng <deng.yo@northeastern.edu>
2023-07-08 22:53:17 -0400

    Build up token authentication middlewares

---
+++ server/middleware/auth.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+
+exports.loginVerify = async function (req, res, next) {
+      try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            if (decoded) return next();
+            else return next({
+                  status: 401,
+                  message: 'Please sign in!'
+            });
+      } catch (err) {
+            return next(err);
+      }
+}
+
+exports.userVerify = async function (req, res, next) {
+      try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            if (decoded && decoded.id === req.params.id) return next();
+            else return next({
+                  status: 401,
+                  message: 'Unauthorized'
+            })
+      } catch (err) {
+            return next(err);
+      }
+}
+
+exports.vendorVerify = async function (req, res, next) {
+      try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            if (decoded && decoded.category === 'VENDOR') return next();
+            else return next({
+                  status: 401,
+                  message: 'Unauthorized'
+            })
+      } catch (err) {
+            return next(err);
+      }
+}
