c5eea89d91395df958f104e52848e3c326928171
dyy <39893924+YongyanDeng@users.noreply.github.com>
2023-07-09 12:51:17 -0400

    revert auth.js update

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -32,7 +32,7 @@ exports.vendorVerify = async function (req, res, next) {
       try {
             const token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            if (decoded && decoded.id === req.params.id && decoded.category === 'VENDOR') return next();
+            if (decoded && decoded.category === 'VENDOR') return next();
             else return next({
                   status: 401,
                   message: 'Unauthorized'
