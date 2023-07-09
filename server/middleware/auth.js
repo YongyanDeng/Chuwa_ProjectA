c61787e3afcc21dbbdf01fd02daae18063b46216
Yongyan Deng <deng.yo@northeastern.edu>
2023-07-09 14:54:20 -0400

    set js' tab size to 4

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,43 +1,46 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 exports.loginVerify = async function (req, res, next) {
-      try {
-            const token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            if (decoded) return next();
-            else return next({
-                  status: 401,
-                  message: 'Please sign in!'
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (decoded) return next();
+        else
+            return next({
+                status: 401,
+                message: "Please sign in!",
             });
-      } catch (err) {
-            return next(err);
-      }
-}
+    } catch (err) {
+        return next(err);
+    }
+};
 
 exports.userVerify = async function (req, res, next) {
-      try {
-            const token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            if (decoded && decoded.id === req.params.id) return next();
-            else return next({
-                  status: 401,
-                  message: 'Unauthorized'
-            })
-      } catch (err) {
-            return next(err);
-      }
-}
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (decoded && decoded.id === req.params.id) return next();
+        else
+            return next({
+                status: 401,
+                message: "Unauthorized",
+            });
+    } catch (err) {
+        return next(err);
+    }
+};
 
 exports.vendorVerify = async function (req, res, next) {
-      try {
-            const token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            if (decoded && decoded.category === 'VENDOR') return next();
-            else return next({
-                  status: 401,
-                  message: 'Unauthorized'
-            })
-      } catch (err) {
-            return next(err);
-      }
-}
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (decoded && decoded.category === "VENDOR") return next();
+        else
+            return next({
+                status: 401,
+                message: "Unauthorized",
+            });
+    } catch (err) {
+        return next(err);
+    }
+};
