74885a0c5ed41c57df19d58f48640ad9dbc861c9
Yongyan Deng <deng.yo@northeastern.edu>
2023-07-19 00:44:46 -0400

    Update UI of product detail page

--- client/src/pages/ProductDetail/index.jsx
+++ client/src/pages/ProductDetail/index.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
-import { Card, Col, Row, Space, Image, Typography, Button, message, InputNumber } from "antd";
+import { Card, Col, Row, Space, Image, Typography, Button, message, InputNumber, Tag } from "antd";
+import {
+    MinusOutlined,
+    PlusOutlined,
+    CloseCircleOutlined,
+    CheckCircleOutlined,
+} from "@ant-design/icons";
 
 import { useSelector, useDispatch } from "react-redux";
 import { fetchOneProductAction } from "app/productSlice";
 import { useNavigate, useParams } from "react-router-dom";
 import { updateCartProduct, addCartProduct, getCart } from "app/userSlice";
-import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
+
 import AddToCartButton from "components/Product/AddToCart";
 import styles from "./style.module.css";
 
@@ -15,9 +21,11 @@ function ProductDetail() {
     const { user } = useSelector((state) => state.user);
     const { productId } = useParams();
     const navigate = useNavigate();
+
     useEffect(() => {
         dispatch(fetchOneProductAction(productId));
     }, [productId]);
+
     const editButtonClick = (product) => (e) => {
         if ((user.category === "VENDOR") & (product.createdBy === user.id)) {
             dispatch(fetchOneProductAction(product._id)).then(() => {
@@ -27,13 +35,17 @@ function ProductDetail() {
             message.error(`Unauthorized`);
         }
     };
+
     return (
-        <div className={styles.container}>
+        <>
             {oneProduct ? (
-                <div>
-                    <Row gutter={6} justify="center">
-                        <Col span={12}>
-                            <Card
+                <div className={styles.product_detail}>
+                    <Typography.Title level={2} className={styles.title}>
+                        Products Detail
+                    </Typography.Title>
+                    <Row className={styles.container} gutter={6} justify="center">
+                        <Col span={12} className={styles.col_left}>
+                            {/* <Card
                                 title={oneProduct.name}
                                 bordered={false}
                                 bodyStyle={{ padding: 0 }}
@@ -46,10 +58,53 @@ function ProductDetail() {
                                     />
                                 }
                                 className={styles.card}
-                            ></Card>
+                            ></Card> */}
+                            <Image
+                                className={styles.itemCardBadge}
+                                width={"550px"}
+                                height={"550px"}
+                                src={oneProduct.imageUrl}
+                                alt={oneProduct.name}
+                            />
                         </Col>
-                        <Col span={12}>
-                            <Card
+                        <Col span={12} className={styles.col_right}>
+                            <Typography.Text className={styles.product_category}>
+                                {oneProduct.category}
+                            </Typography.Text>
+                            <Typography.Title level={2} className={styles.product_name}>
+                                {oneProduct.name}
+                            </Typography.Title>
+                            <div className={styles.product_price_tag}>
+                                <Typography.Title level={3} className={styles.product_price}>
+                                    {`$${oneProduct.price}`}
+                                </Typography.Title>
+                                {oneProduct.stockNum === 0 ? (
+                                    <Tag icon={<CloseCircleOutlined />} color="error">
+                                        Out of Stock
+                                    </Tag>
+                                ) : (
+                                    <Tag icon={<CheckCircleOutlined />} color="success">
+                                        Adequate inventory
+                                    </Tag>
+                                )}
+                            </div>
+                            <Typography.Paragraph className={styles.product_description}>
+                                {oneProduct.description}
+                            </Typography.Paragraph>
+                            <div className={styles.buttons}>
+                                <AddToCartButton product={oneProduct} user={user} />
+                                {user.category === "VENDOR" && (
+                                    <Button
+                                        className={styles.edit_btn}
+                                        type="primary"
+                                        onClick={editButtonClick(oneProduct)}
+                                    >
+                                        Edit
+                                    </Button>
+                                )}
+                            </div>
+
+                            {/* <Card
                                 title={oneProduct.name}
                                 key={productId}
                                 actions={[
@@ -89,14 +144,14 @@ function ProductDetail() {
                                         </Typography.Paragraph>
                                     }
                                 ></Card.Meta>
-                            </Card>
+                            </Card> */}
                         </Col>
                     </Row>
                 </div>
             ) : (
                 <p>Loading...</p>
             )}
-        </div>
+        </>
     );
 }
 
